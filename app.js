30a6cc1743f86e5dff89382ed36b5bd359b2ef92
shobhit29075 <shobhit29075@gmail.com>
2023-11-01 23:52:02 -0400

    Pushing Template to follow, node environment setup

--- app.js
+++ app.js
@@ -7,4 +7,20 @@ var con = sql.createConnection({
     password: "root@123",
     database: "Reyoca_DB"
 });
-
+// TEMPLATE FOR QUERY
+con.connect(function(err) {
+    if (err) throw err;
+    var sql = "SELECT * FROM employee_details WHERE employee_id IN ("+ids+")";
+    con.query(sql, function (err, result) {
+        if (err){
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Requested record not cound the the database. Change the primary key');
+            return;
+        };
+        // console.log(typeof result);
+        result=JSON.stringify(result)
+        // console.log(result)
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(result);
+    });
+});
